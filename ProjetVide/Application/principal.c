c736540b3d5e47e7b4a5fad7040914895df7ca24
Elsahindi <hindi@insa-toulouse.fr>
2025-03-25 08:09:01 +0100

    tp1-2 bug fixed

--- ProjetVide/Application/principal.c
+++ ProjetVide/Application/principal.c
@@ -1,6 +1,9 @@
 #include "stm32f10x.h"
+#include "stdbool.h"
 
 int counter = 0;
+int counter1 = 0;
+int counter2 = 0;
 
 // Each register 32 bits
 // [xPSR, PC, LR, R12, R3, R2, R1, R0]
@@ -10,81 +13,82 @@ int PSP = 0;
 // NOTE : Stack is 0x5000 in size and starts at 0x200000000
 // stack pointers start at address ( remember stack decrements ) 
 uint32_t * stack1 = (uint32_t *) 0x20004000 ;
-uint32_t * stack2 = (uint32_t *) 0x20005000 ;
+uint32_t * stack2 = (uint32_t *) 0x20003000 ;
+
+bool setup = false;
 
 int current_process;
 
 void SysTick_Handler ( void ) {
-		if (current_process == 0) { // process 1
-			__set_PSP( (uint32_t) stack1);
+		if (!setup) {
+			__set_PSP((uint32_t) stack1);
+			setup = true;
 			current_process = 1;
+			return;
+		}
+	
+		if (current_process == 1) { // process 1
+			stack1 = (uint32_t *) __get_PSP();
+			__set_PSP((uint32_t) stack2);
+			current_process = 2;
 		}
 		else{ // process 2 
-			__set_PSP( (uint32_t) stack2);
-			current_process = 0;
+			stack2 = (uint32_t *) __get_PSP();
+			__set_PSP( (uint32_t)stack1);
+			current_process = 1;
 		}
-		
 		counter ++;
 }
 
 void dummy1 ( void ) {
 	while (1){
+		counter1++;
 	}
 }
 
 void dummy2 ( void ) {
 	while (1){
+		counter2++;
 	}
 }
 
 int main ( void )
 {
-	RCC->APB2ENR |= (0x01 << 2) | (0x01 << 3) | (0x01 << 4) ;
-	
-	GPIOA->CRL &= ~(0xF<<4) ;
-	GPIOA->CRL |= (0x2<<4) ;
-	
-	// check if not already in unpriviledge mode, Process stack
-	if (!(__get_CONTROL()&(0b11))){
-		__set_CONTROL(0b11);
-	}
-	
 	// start at process 1
 	current_process = 0;
 	
 	// xPSR 0x1000000
-	*stack1 = 0x1000000;
-	*stack2 = 0x1000000;
+	*(stack1 + 7) = 0x1000000;
+	*(stack2 + 7) = 0x1000000;
 	
 	// PC to address of respective function
-	*(stack1 + 1) =  (uint32_t) &dummy1;
-	*(stack2 + 1) =  (uint32_t) &dummy2;
+	*(stack1 + 6) =  (uint32_t) &dummy1;
+	*(stack2 + 6) =  (uint32_t) &dummy2;
 	
 	// LR back to thread mode with process stack 0xFFFFFFFD 
-	*(stack1 + 2) =  (uint32_t) 0xFFFFFFFD;
-	*(stack2 + 2) =  (uint32_t) 0xFFFFFFFD;
+	*(stack1 + 5) =  (uint32_t) 0xFFFFFFFD;
+	*(stack2 + 5) =  (uint32_t) 0xFFFFFFFD;
 	
 	// initialise R0-R3 and R12 to 0x0
-	for (int i=3; i<8; i++){
+	for (int i=0; i<5; i++){
 		*(stack1 + i) = 0x0;
 		*(stack2 + i) = 0x0;
 	}
 	
-	// ensruing that systick is disabled
-	SysTick ->CTRL = 0x0;
-	
 	// Systick will reload after 1ms
 	SysTick -> LOAD = (SystemCoreClock / 1000U);
 	
-	// loaded value after reaching zero
-	SysTick ->VAL = 10;
-	
 	// enable interruption
-	SysTick -> CTRL |= SysTick_CTRL_TICKINT_Msk;
+	SysTick -> CTRL |= SysTick_CTRL_TICKINT;
 	
 	// enabling systick
-	SysTick -> CTRL |= SysTick_CTRL_CLKSOURCE_Msk;
-	SysTick -> CTRL |= SysTick_CTRL_ENABLE_Msk  ;
+	SysTick -> CTRL |= SysTick_CTRL_CLKSOURCE;
+	SysTick -> CTRL |= SysTick_CTRL_ENABLE  ;
+	
+	// check if not already in unpriviledge mode, Process stack
+	uint32_t ctrl = __get_CONTROL();
+	ctrl |= 0b11;
+	__set_CONTROL(ctrl);
 	
 	
 	while (1)
